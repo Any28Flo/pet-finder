5afa3fef4337a33da3a96584070e74cc48c05b13
ErikaFlores <foglzerika@gmail.com>
2023-09-05 16:25:03 -0600

    feat | Create skeleton Login

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,7 +1,38 @@
+import styled from "styled-components";
+
+const LoginWrapper = styled.main`
+    height: 100vh;
+    width: 100%;
+    display: flex;
+    
+    align-items: center;
+    justify-content: center;
+`;
+const FormWrapper = styled.form`
+  margin: 6rem;
+  padding:10rem;
+  display: flex;
+  flex-direction: column;
+  background-color: red;
+`;
 
 const Login = () => {
   return (
-    <div>Login</div>
+    <LoginWrapper>
+      <FormWrapper>
+          <legend>Log In</legend>
+            <div className="form-group">
+              <label htmlFor="name">Name:</label>
+              <input type="text" name="name" id="name" />
+            </div>
+            <div className="form-group">
+              <label htmlFor="email">Email:</label>
+              <input type="email" name="email" id="email" />
+            </div>
+          <button type="submit">Login</button>
+     
+      </FormWrapper>
+    </LoginWrapper>
   )
 }
 
